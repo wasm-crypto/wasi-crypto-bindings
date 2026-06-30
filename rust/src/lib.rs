c2e83569f1e9c4e541bc778400097efb238d370f
Frank Denis <github@pureftpd.org>
2026-07-01 00:31:31 +0200

    Expose algorithm_type in the public interface

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,12 +2,15 @@ mod asymmetric_common;
 mod common;
 mod raw;
 
+pub use common::algorithm_type;
+
 pub mod error;
 pub mod kx;
 pub mod signatures;
 pub mod symmetric;
 
 pub mod prelude {
+    pub use crate::algorithm_type;
     pub use crate::error::Error as WasiCryptoError;
     pub use crate::kx::*;
     pub use crate::signatures::*;
